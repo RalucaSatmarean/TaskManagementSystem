84e8e1038c6758e0827e150517aa2e297605a0a4
raluca Carafa <raluca.carafa@gmail.com>
2022-11-06 14:19:54 +0200

    Added Controllers and CreateProject view

---
+++ TaskManagementSystem/Controllers/ProjectController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskManagementSystem.Data;
+
+namespace TaskManagementSystem.Controllers
+{
+    public class ProjectController : Controller
+    {
+       private Repository.ProjectRepository _repository;
+
+        public ProjectController (ApplicationDbContext dbContext)
+        {
+            _repository = new Repository.ProjectRepository(dbContext);
+        }
+
+
+        // GET: ProjectController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ProjectController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProjectController/Create
+        public ActionResult Create()
+        {
+            return View("CreateProject");
+        }
+
+        // POST: ProjectController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                Models.ProjectModel model = new Models.ProjectModel();
+
+                var task = TryUpdateModelAsync(model);
+                task.Wait();
+                if (task.Result)
+                {
+                    _repository.InsertProject(model);
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View("CreateProject");
+            }
+        }
+
+        // GET: ProjectController/Edit/5
+        public ActionResult Edit(Guid id)
+        {
+            return View();
+        }
+
+        // POST: ProjectController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Guid id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProjectController/Delete/5
+        public ActionResult Delete(Guid id)
+        {
+            return View();
+        }
+
+        // POST: ProjectController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Guid id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
